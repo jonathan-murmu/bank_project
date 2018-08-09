a7efeb316a1dda319487d39bfee9748baecb358e
jonathanmurmu <jonathan.murmu@mindfiresolutions.com>
2018-08-10 00:01:18 +0530

    modified settings

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -24,7 +24,8 @@ load_dotenv(dotenv_path=BASE_DIR+'/bank_project/.env')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+# SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY='esto2jnc-ids5+fa0+y()5te0y#lm-+#59x__flged(yos9ssi'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
