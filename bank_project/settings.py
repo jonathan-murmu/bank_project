abd42834324a8889f1453102e8acef5f97cbb9d5
jonathanmurmu <jonathan.murmu@mindfiresolutions.com>
2018-08-09 20:48:32 +0530

    allowed host, propfile and modified requirements

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysterious-shelf-59883.herokuapp.com', 'localhost']
 
 
 # Application definition
